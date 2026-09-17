b3ab5bc11e3b1a3d809b73ea4d5dbedd61d63fc6
thanharess <116294355+thanharess@users.noreply.github.com>
2026-09-17 18:22:37 +0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAD_Adin
+{
+    public class Class1
+    {
+    }
+}
